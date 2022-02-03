e8180af3fe08e8bf91f301e294518590821e3b52
J0kk3 <joakim_88@msn.com>
2022-02-03 14:04:02 +0100

    Folder structure & initial exercise setup

---
+++ Övningsuppgifter/js2_API_Async_Await/js/script.js
@@ -0,0 +1,28 @@
+/*Övning 1: Skapa ett promise som
+    1. Genererar ett random nummer mellan 0 och 10
+    2. Anropar resolved-funktionen om numret är udda och skickar med numret som argument
+    3. Anropar reject-funktionen om numret är jämnt och skickar med ett felmeddelande som argument
+När Promiset är resolved, ska value visas i ett h1 element.
+När Promiset blir rejected, ska felmeddelandet loggas i konsolen
+*/
+
+
+/*
+Övning 2: Skapa ett promise som tar 3 sekunder på sig att
+    1. Generera ett random nummer mellan 0 och 50
+    2. Anropa resolved-funktionen om numret är mellan 10 och 30
+    3. Eller anropa reject-funktionen om numret inte är mellan 10 och 30
+När promiset blir resolved, ska value visas i ett h1-element. 
+När promiset blir rejected, ska felmeddelandet loggas i konsolen
+*/
+
+
+/*
+3) Skapa två promises som båda tar random tid mellan 0 och 5 sekunder på sig att
+    1. Generera ett random nummer mellan 0 och 50
+    2. Anropa resolved-funktionen om numret är mer än 5
+    3. Eller anropa reject-funktionen om numret är mindre än 5
+När promiset blir resolved, ska value samt vilket promise det kommer ifrån visas i ett h1-element. 
+När promiset blir rejected, ska felmeddelandet samt vilket promise det kom ifrån loggas i konsolen.
+*/
+
