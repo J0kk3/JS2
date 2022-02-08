1c77cb46be81a80d6a1aec03dcae0943626d122d
J0kk3 <joakim_88@msn.com>
2022-02-08 12:03:19 +0100

    Firebase uppgiften klar

--- Övningsuppgifter/Firebase/js/script.js
+++ Övningsuppgifter/Firebase/js/script.js
@@ -1,46 +1,41 @@
 const url = "https://productassignment-56bbb-default-rtdb.europe-west1.firebasedatabase.app/.json";
-const kryddaUrl = "https://productassignment-56bbb-default-rtdb.europe-west1.firebasedatabase.app/Krydda.json";
-const iKaffeUrl = "https://console.firebase.google.com/u/0/project/productassignment-56bbb/database/productassignment-56bbb-default-rtdb/data/iKaffe.json";
-const matUrl = "https://productassignment-56bbb-default-rtdb.europe-west1.firebasedatabase.app/mat.json";
-const äppleUrl = "https://productassignment-56bbb-default-rtdb.europe-west1.firebasedatabase.app/%C3%84pple.json";
-const ölUrl = "https://productassignment-56bbb-default-rtdb.europe-west1.firebasedatabase.app/%C3%96l.json";
+// const kryddaUrl = "https://productassignment-56bbb-default-rtdb.europe-west1.firebasedatabase.app/Krydda.json";
+// const iKaffeUrl = "https://console.firebase.google.com/u/0/project/productassignment-56bbb/database/productassignment-56bbb-default-rtdb/data/iKaffe.json";
+// const matUrl = "https://productassignment-56bbb-default-rtdb.europe-west1.firebasedatabase.app/mat.json";
+// const äppleUrl = "https://productassignment-56bbb-default-rtdb.europe-west1.firebasedatabase.app/%C3%84pple.json";
+// const ölUrl = "https://productassignment-56bbb-default-rtdb.europe-west1.firebasedatabase.app/Öl.json";
 
 fetch(url)
-    .then(r=>r.json())
-    .then(d=>{console.log("Listar alla produkter", d)
-    //Logga endast namnet på varje produkt i konsolen
-    for(const i in d)
-    {
-        console.log(i);
-    }
-});
-
-const namn0 = document.getElementById("namn0");
-fetch(ölUrl)
-    .then(r=>r.json())
-    .then(d=>{d
-        //undefined
-        namn0.innerHTML = ölUrl.namn;
-});
-const namn1 = document.getElementById("namn1");
-const namn2 = document.getElementById("namn2");
-const namn3 = document.getElementById("namn3");
-const namn4 = document.getElementById("namn4");
-
-const märke0 = document.getElementById("märke0");
-const märke1 = document.getElementById("märke1");
-const märke2 = document.getElementById("märke2");
-const märke3 = document.getElementById("märke3");
-const märke4 = document.getElementById("märke4");
-
-const pris0 = document.getElementById("pris0");
-const pris1 = document.getElementById("pris1");
-const pris2 = document.getElementById("pris2");
-const pris3 = document.getElementById("pris3");
-const pris4 = document.getElementById("pris4");
-
-const desc0 = document.getElementById("desc0");
-const desc1 = document.getElementById("desc1");
-const desc2 = document.getElementById("desc2");
-const desc3 = document.getElementById("desc3");
-const desc4 = document.getElementById("desc4");
+    .then(r=>
+        {
+        return r.json();
+        })
+    .then(d=>
+        {
+            console.log(d);
+            for(const key in d)
+            {
+                // console.log(key, typeof key, d[key].url);
+                let div = document.createElement("div");
+                document.body.appendChild(div);
+                div.className = "card";
+                div.style.display="inline-block";
+                let h3 = document.createElement("h3");
+                div.appendChild(h3);
+                h3.innerText=d[key].märke;
+                let p0 = document.createElement("p");
+                div.appendChild(p0);
+                p0.innerText=d[key].namn;
+                let img = document.createElement("img");
+                div.appendChild(img);
+                img.style.width="100px";
+                img.src=d[key].url;
+                let p1 = document.createElement("p");
+                div.appendChild(p1);
+                p1.innerText="pris: " + d[key].pris + "kr";
+                let p2 = document.createElement("p");
+                div.appendChild(p2);
+                p2.innerText=d[key].beskrivning;
+                p2.style.fontSize="10px";
+            }
+        });
