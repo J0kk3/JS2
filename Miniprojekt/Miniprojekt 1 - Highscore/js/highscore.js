539715787a99b527d0d27cd588b83385fd851e51
J0kk3 <joakim_88@msn.com>
2022-02-09 10:13:51 +0100

    genomgång med lösning på highscore

---
+++ Miniprojekt/Miniprojekt 1 - Highscore/js/highscore.js
@@ -0,0 +1,25 @@
+/*
+DB - Array.js
+index       name        score
+0           Cornelia    100
+1           Jonathan    83
+2           Forqan      57
+3           Sara        33
+4           Natta       12
+
+//placeholder variable = newScore = 57
+//for-loop för att kolla score med databasen exempelvis
+//om newScore är samma som ett redan existerande highscore;
+//      så sätts den in under (då poängen inte är slagen)
+/*for(i < array.length)
+{
+    if(newScore == array[i].score)
+    {
+    Plats = i+1;
+    }
+    else if(newScore > array[i].score)
+    {
+        Plats = i;
+    }
+}
+*/
