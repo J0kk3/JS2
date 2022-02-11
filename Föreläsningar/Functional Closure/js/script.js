b4dff80017a4aa9e1c9c7c4fb11b3bac411a11f9
J0kk3 <joakim_88@msn.com>
2022-02-11 12:01:36 +0100

    Lecture: Reset counter

---
+++ Föreläsningar/Functional Closure/js/script.js
@@ -0,0 +1,84 @@
+function sayHello(name){
+    const greeting = `Hello, ${name}`;
+ 
+    function say(){
+        console.log(greeting);
+    };
+    return say;
+}
+
+const sayHelloToEveryone = sayHello('Everyone');
+const sayHelloToTheMoon = sayHello('the Moon');
+ 
+sayHelloToEveryone(); //’Hello, Everyone’
+sayHelloToTheMoon(); //'Hello, the moon'
+
+console.log(sayHelloToEveryone);
+console.log(sayHelloToTheMoon);
+
+console.log("___________Counter med closure____________");
+
+function count()
+{
+    let counter = 0; //Är en del av "Closure"
+
+    function countUp()
+    {
+        return ++counter;
+    }
+    return countUp;
+}
+const myCounter = count();
+
+console.log(myCounter); //Returnerar: function countUp();
+
+console.log(myCounter()); //Returnerar: 1
+console.log(myCounter()); //Returnerar: 2 (för att counter ökar på)
+
+const mySecondCounter = count();
+console.log(mySecondCounter()); //Returnerar: 1
+
+console.log(myCounter()); //Returnerar: 6
+
+const counter0 = count();
+const counter1 = count();
+
+document.querySelector("#b1").addEventListener("click", (e)=>
+{
+    e.target.innerText = counter0();
+});
+document.querySelector("#b2").addEventListener("click", (e)=>
+{
+    e.target.innerText = counter1();
+});
+console.log("___________Reset Counter____________");
+
+function count()
+{
+    let counter = 0;
+
+    function countUp()
+    {
+        return ++counter;
+    }
+
+    function reset()
+    {
+        counter = 0;
+    }
+    return{
+        countUp: countUp,
+        reset: reset
+    };
+}
+const myCounter = count();
+const mySecondCounter = count();
+console.log(mySecondCounter);
+console.log(myCounter);
+console.log(mySecondCounter.countUp());
+console.log(myCounter.countUp());
+console.log(myCounter.countUp());
+console.log(myCounter.countUp());
+console.log(myCounter.countUp());
+myCounter.reset();
+console.log(myCounter.countUp());
