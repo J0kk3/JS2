b1e997ab16e60058c6f2162577307f4ac714189c
J0kk3 <joakim_88@msn.com>
2022-02-16 10:43:47 +0100

    Lecture: Modules

---
+++ Föreläsningar/Moduler/main.js
@@ -0,0 +1,6 @@
+//Måste vara längst upp
+import { animal1, animal2 } from "./modules/animals.js";
+import { displayRandom } from "./modules/display.js";
+
+displayRandom(animal1);
+displayRandom(animal2);
---
+++ Föreläsningar/Moduler/modules/animals.js
@@ -0,0 +1,14 @@
+const animal1 = {
+    species: 'Elephant',
+    name: 'Dumbo',
+    likes: ['Mud', 'Bathing', 'Flapping ears']
+}
+ 
+const animal2 = {
+    species: 'Squirrel',
+    name: 'Killer Squirrel',
+    likes: ['Biting', 'Nibbling', 'Teeth']
+}
+
+//Måste vara längst ner
+export {animal1, animal2};
