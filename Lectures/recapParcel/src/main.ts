a6ff111e7abbad386529e85e65d28246179b66ca
J0kk3 <joakim_88@msn.com>
2022-03-16 11:38:05 +0100

    Lecture Parcel Recap added additional images

--- Lectures/recapParcel/src/main.ts
+++ Lectures/recapParcel/src/main.ts
@@ -1,7 +1,17 @@
-const img = document.createElement("img");
-document.body.appendChild(img);
+// const img = document.createElement("img");
+// document.body.appendChild(img);
 //Detta funkar inte
 // img.src="./img/dragon.jpg";
 
-const imgUrl = new URL("./img/dragon.jpg", import.meta.url);
-img.src = imgUrl.href; //kan använda .toString(); || .href;
+//const imgUrl = new URL("./img/dragon.jpg", import.meta.url);
+//img.src = imgUrl.href; //kan använda .toString(); || .href;
+
+const imgNames = ["ballongvisp", "dragon", "evilcat"];
+
+for(const el of imgNames)
+{
+    const img = document.createElement("img");
+    document.body.appendChild(img);
+    const imgUrl = new URL(`.img/${el}.jpg`, import.meta.url);
+    img.src = imgUrl.href;
+}
