0d821f12c134d9db27f65089460ce3db5d5e7a96
J0kk3 <joakim_88@msn.com>
2022-03-22 11:16:33 +0100

    Project - Chatt - init

---
+++ Miniprojekt/Miniprojekt-5-chatt/FE21-Joakim-sjostrom-chatt/src/main.ts
@@ -0,0 +1,14 @@
+/*
+Firebase
+Max 25 meddelanden i db
+läggs till nytt - senaste ska tas bort
+meddelande ska innehålla textinnehåll & namn på postern
+
+Webappen
+först fylla i namn
+visa senaste 25 meddelanden
+presenteras i kronologisk ordning
+lägga till meddelande via formulär
+uppdateras direkt med nya meddelanden
+ska kunna ta bort sina egna meddelanden
+*/
